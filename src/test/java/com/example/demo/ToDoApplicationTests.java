fe30e97679bfb2bcce887547dbe38795111e73a1
Sriram Dhanaraj <sriram_dhanaraj@epam.com>
2025-01-04 10:50:22 +0530

    Dockerfile committed

--- src/test/java/com/example/demo/ToDoApplicationTests.java
+++ src/test/java/com/example/demo/ToDoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.sriram.my.ToDoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= ToDoApplication.class)
 class ToDoApplicationTests {
 
 	@Test
